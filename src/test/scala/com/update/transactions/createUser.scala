6b7cbfce585b00e4f24f2cd2a00e83d54153f13b
Aninda Mukherjee <aninda_mukherjee@hotmail.com>
2021-04-29 00:28:03 -0700

    updated comments and cleaup work.

--- src/test/scala/com/update/transactions/createUser.scala
+++ src/test/scala/com/update/transactions/createUser.scala
@@ -4,6 +4,23 @@ import com.github.javafaker.Faker
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+/**
+ * Test Simulation for createUser API.
+ *
+ * POST call
+ * Below is the endpoint and payload of this API:
+ *
+ *  curl --location --request POST 'http://localhost:8080/user/v1/create' \
+ *  --header 'Content-Type: application/json' \
+ *  --data-raw '{
+ *    "firstName":"Mickey",
+ *    "lastName":"Mouse",
+ *    "email":"mickey@gmail.com"
+ *  }'
+ *
+ * @author XXXXXX
+ */
+
 object createUser {
 
   val logger = org.slf4j.LoggerFactory.getLogger("shellPerfLogger")
@@ -17,7 +34,8 @@ object createUser {
           val faker = new Faker
           val firstName=faker.name.firstName()
           val lastName = faker.name.lastName()
-          val email = faker.name.username() + "@mail.com"
+          val  r = new scala.util.Random;
+          val email = faker.name.username() +r + "@mail"+r + ".com"
 
           var payload = "{\"firstName\":\"" + firstName + "\",\"lastName\":\"" + lastName + "\",\"email\":\"" + email + "\"}";
           session.set("upgrade_tid",headerString).set("payload", payload)
@@ -28,15 +46,16 @@ object createUser {
           .header("Content-Type", "application/json")
           .header("Accept", "*/*")
           .header("Accept-Encoding", "gzip, deflate")
-//          .header("Connection", "keep-alive")
-//          .header("tid","222-22-222")
-          .body(StringBody("""${payload}"""))
+          .body(StringBody("""${payload}""")) // Payload
 
+          //Assertion to validate page load is successful.
           .check(status.in(201))
           .check(responseTimeInMillis.saveAs("execLatency"))
           .check(bodyString.saveAs("responsePayload"))
           .check(jsonPath("$.id").saveAs("p_uuid"))
       )
+
+         // Logs for debugging purpose
         .exec(session=>{
           val response=session("responsePayload").as[String];
           val uuid=session("p_uuid").as[String];
